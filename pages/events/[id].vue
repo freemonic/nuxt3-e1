071bfd0397a91f5d4d67179de0e5956234c4e25c
freemonic <luckmusk@yahoo.com>
2024-05-23 18:15:34 +0600

    A dynamic parameter was intercepted.

--- pages/events/[id].vue
+++ pages/events/[id].vue
@@ -1,3 +1,12 @@
+<script setup lang="ts"> 
+    import { useRoute } from "vue-router"
+
+    const route = useRoute()
+    console.log(route)
+    console.log(route.params)
+    console.log(route.params.id)
+</script>
+
 <template>
     <div>event id: {{ $route.params.id }}</div>
 </template>
