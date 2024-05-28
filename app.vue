357bb2b600d759a6be12e647a149173a545c3cd0
freemonic <luckmusk@yahoo.com>
2024-05-28 09:45:21 +0600

    Created Alert component.

--- app.vue
+++ app.vue
@@ -1,14 +1,6 @@
 <template>
   <div>
     <NuxtLayout>
-      <header>
-        <ul>
-          <li>
-            <NuxtLink to="/">Home</NuxtLink>
-            <NuxtLink to="/events">Events</NuxtLink>
-          </li>
-        </ul>
-      </header>
       <NuxtPage/>
     </NuxtLayout>
   </div>
